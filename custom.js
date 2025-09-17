9d4012f3027d1c042fe9c20f511fbf64f1b367c2
Alissa Marie Rowell <amrowe38@louisville.edu>
2025-09-17 05:59:44 +0000

    refactor: Update custom.js to clarify its purpose and add a comment in index.html to restrict additional scripts

--- custom.js
+++ custom.js
@@ -1,3 +1,3 @@
-// (This file is intentionally left empty. All previous code has been deleted.)
+// This file is intentionally left empty or can be used for future static enhancements.
 // This file is intentionally left empty or can be used for future static enhancements.
 // Otherwise, leave this file empty or with only essential code.
